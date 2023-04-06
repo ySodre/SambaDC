2c62487d71e41c4437d7981c9ed6572e55b60946
ySodre <sodre-yuri@hotmail.com>
2023-04-06 11:40:56 -0300

    Alteração no OutPUT - Color

--- SambaDC.sh
+++ SambaDC.sh
@@ -1,48 +1,49 @@
 #!/bin/bash
-echo -e "\033[32mInstalando pacotes necessários\033[0m"
+echo -e "\033[32mATUALIZANDO O SISTEMA...\033[0m"
 
-apt update -y && apt dist-upgrade -y 
-apt install samba -y 
-apt install winbind -y 
+apt update -y && apt dist-upgrade -y  > /dev/null 2>&1
+echo -e "\033[32mINSTALANDO SAMBA...\033[0m"
+apt install samba -y  > /dev/null 2>&1
+echo -e "\033[32mINSTALANDO WINBIND\033[0m"
+apt install winbind -y  > /dev/null 2>&1
 #apt install krb5-user -y 1>/dev/null
-echo -e "\033[32mInstalando Webmin...\033[0m"
-wget http://prdownloads.sourceforge.net/webadmin/webmin_2.000_all.deb
+echo -e "\033[32mINSTALANDO WEBMIN...\033[0m" 
+wget http://prdownloads.sourceforge.net/webadmin/webmin_2.000_all.deb > /dev/null 2>&1
 apt install -y perl libnet-ssleay-perl openssl libauthen-pam-perl \
 libpam-runtime libio-pty-perl apt-show-versions python unzip shared-mime-info > /dev/null 2>&1
 dpkg --install webmin_2.000_all.deb > /dev/null 2>&1
 
-echo -e "\033[32mDesabilitando serviços desnecessários...\033[0m"
+echo -e "\033[32mDESABILITANDO SERVIÇOS DESNECESSÁRIOS...\033[0m"
 systemctl stop smbd nmbd > /dev/null 2>&1
 systemctl disable smbd nmbd > /dev/null 2>&1
 systemctl stop systemd-networkd > /dev/null 2>&1
 systemctl disable systemd-networkd > /dev/null 2>&1
 
-echo -e "\033[32mRemovendo configuração padrão do samba...\033[0m"
-rm /etc/krb5.conf
+echo -e "\033[32mREMOVENDO ARQUIVOS DE CONFIGURAÇÃO PADRÃO\033[0m"
 rm /etc/samba/smb.conf
 
-echo -e "\033[32mProvisionando o domínio...\033[0m"
-echo "Digite o nome do realm. Exemplo: zeta.local"
+echo -e "\033[32mPROVISIONANDO O DOMÍNIO...\033[0m"
+echo "\033[32mDIGITE O NOME DO DOMÍNIO COMPLETO EX: zeta.local\033[0m"
 read realm
-echo -e "\033[32mDigite o nome do domínio. Exemplo: zeta\033[0m"
+echo -e "\033[32mDIGITE O NOME DO DOMÍNIO APENAS O PRIMEIRO NOME EX: zeta\033[0m"
 read domain
-echo -e "\033[32mDigite a senha de administrador: \033[0m"
+echo -e "\033[32mDEFINA UMA SENHA DE ADMINISTRADOR: \033[0m"
 read password
 samba-tool domain provision --use-rfc2307 --realm=$realm --domain=$domain --adminpass=$password --server-role=dc --dns-backend=SAMBA_INTERNAL
 
 #echo "Copiando arquivos de configurações necessários"
 #cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
 
-echo -e "\033[32mHabilitando o serviço\033[0m"
+echo -e "\033[32mHABILITANDO O SERVIÇO...\033[0m"
 systemctl unmask samba-ad-dc > /dev/null 2>&1
 systemctl enable samba-ad-dc > /dev/null 2>&1
 systemctl stop winbind > /dev/null 2>&1
 systemctl start samba-ad-dc > /dev/null 2>&1
 
-echo -e "\033[32mAjustando DNS\033[0m"
+echo -e "\033[32mAJUSTANDO DNS...\033[0m"
 echo "nameserver 127.0.0.1" > /etc/resolv.conf
 
-echo -e "\033[32mAdicionando configurações necessárias\033[0m"
+echo -e "\033[32mFINALIZANDO CONFIGURAÇÕES...\033[0m"
 sudo sed -i '/^\        idmap_ldb:use rfc2307 = yes$/a\
 \
         #Configurações de Log\
@@ -67,4 +68,6 @@ sudo sed -i '/^\        idmap_ldb:use rfc2307 = yes$/a\
         #Configuração de restrição de pastas\
         veto files = /*.mp3\
 \
-' /home/vagrant/teste.txt
+' /etc/samba/smb.conf
+
+echo -e "\033[32mINSTALAÇÃO FINALIZADA!!!\033[0m"
